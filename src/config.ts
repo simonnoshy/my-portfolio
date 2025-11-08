9b4cab6439c105d5469bf64613029a2bb5f7b397
Simon Noshy <noshysimon@gmail.com>
2025-11-09 01:55:33 +0200

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,94 @@
+export const siteConfig = {
+  name: "Sandi Noshy",
+  title: "Junior Software Engineer",
+  description: "Portfolio website of Ryan Fitzgerald",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "noshysamir17@gmail.com",
+    linkedin: "https://www.linkedin.com/in/sandi-noshy-988503320/",
+    twitter: "https://x.com/sandiinoshy15",
+    github: "https://github.com/Sandi784-boot",
+  },
+ aboutMe: `
+Passionate Frontend Developer based in Alexandria with experience as a Freelance Web Developer and a Self-Improvement Teacher for Children.
+
+Worked on projects including Library System (Java & JavaFX), Abana (personal website for an Italian client), and Creativto (programming website with HTML, CSS, JS & Bootstrap).
+
+Skilled in HTML, CSS, JavaScript, Bootstrap, and Microsoft Office, with strengths in teamwork, time management, and working under pressure.
+`,
+
+  skills: ["Javascript", "React", "Laravel", "Python", "Shopify"],
+  projects: [
+    {
+      name: "Abana – Personal Website",
+      description:
+        "Abana – A clean and responsive personal website built for an Italian client, designed with HTML, CSS and JavaScript to deliver a seamless experience on mobile and desktop.",
+      link: "https://abana-pulizia.netlify.app/",
+      skills: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      name: "Creativto – Programming Website",
+      description:
+        "A personal website built with HTML, CSS, JavaScript, and Bootstrap, providing programming tips, resources for beginners, and interactive features.",
+      link: "https://hungry-kirch-0329dc.netlify.app/",
+      skills: ["HTML", "CSS", "JavaScript"],
+    },
+
+  ],
+  experience: [
+    {
+      company: "Freelanceer",
+      title: "Junior Software Engineer",
+      dateRange: "Jan 2023 - Present",
+      bullets: [
+        "Built responsive and interactive websites using HTML, CSS, and JavaScript",
+        "Worked on client projects, ensuring mobile-friendly and user-centric designs",
+        "Collaborated with others and improved code quality through feedback"
+      ],
+    },
+    {
+      company: "Freelanceer",
+      title: "Self-Improvement Teacher for Children",
+      dateRange: "Jun 2023 - present",
+    bullets: [
+  "Planned and delivered interactive lessons to help children build confidence and learn effectively",
+  "Created engaging activities to encourage participation and learning",
+  "Developed organizational and communication skills through teaching experience",
+],
+    },
+    {
+      company: " Freelanceer",
+      title: "Frontend Developer",
+      dateRange: "Aug 2023 - present",
+    bullets: [
+  "Built responsive and interactive websites using HTML, CSS, JavaScript, and Bootstrap",
+  "Designed clean and user-friendly layouts for personal and client projects",
+  "Ensured websites are fully responsive and work seamlessly on mobile and desktop",
+],
+    },
+  ],
+  education: [
+    {
+   school: "Alexandria University, Faculty of Science, Computer Science Department",
+degree: "Bachelor of Computer Science",
+dateRange: "Sept 2022 - Jun 2026",
+achievements: [
+  "Completed coursework and projects related to web development",
+        "Graduated with 3.23 GPA", 
+      "Gained foundational skills in programming, problem-solving, and UI design",
+],
+
+    },
+    {
+     school: "The National Telecommunication Institute (NTI)",
+degree: "React and Web Design Course",
+dateRange: "2019",
+achievements: [
+  "Completed 120+ hours of coursework",
+  "Built 10+ projects",
+  "Specialized in React and web design",
+],
+
+    },
+  ],
+};
